9dd71317dffe16fcaa45d588d247b263619f7566
Nuttaphon Ratsameeraweemat <nuttaphon.rat@hotmail.com>
2019-08-27 12:56:37 +0700

    Update -Role Route

---
+++ HelpDesk.Api/Controllers/RoleController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelpDesk.Bll.Components;
+using HelpDesk.Bll.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDesk.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+    public class RoleController : ControllerBase
+    {
+        #region [Fields]
+
+        #endregion
+
+        #region [Constructors]
+
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="RoleController" /> class.
+        /// </summary>
+        /// <param name="register"></param>
+        public RoleController()
+        {
+
+        }
+
+        #endregion
+
+        #region [Methods]
+
+        [HttpGet]
+        [Route("GetRole")]
+        public IActionResult ValidateEmail()
+        {
+            return Ok(new List<ValueHelpViewModel>
+            {
+                new ValueHelpViewModel{ ValueKey = ConstantValue.UserTypeUser, ValueText = "User" },
+                new ValueHelpViewModel{ ValueKey = ConstantValue.UserTypeAdmin, ValueText = "Admin" }
+            });
+        }
+
+        #endregion
+    }
+}
